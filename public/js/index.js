f86e0b9650e667b5e3b74aaa9e424d85bed393f0
piyush maharana <piyush.maharana@hashtaag.com>
2017-10-05 18:55:32 +0530

    Added geolaction feature

--- public/js/index.js
+++ public/js/index.js
@@ -35,6 +35,15 @@ socket.on('newMessage',function (message) {
 //   console.log('Got it',data);
 // });
 
+socket.on('newLocationMessage',function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+  li.text(`${message.from}:`);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit',function (e) {
   e.preventDefault();
 
@@ -45,3 +54,20 @@ jQuery('#message-form').on('submit',function (e) {
 
   });
 })
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    //console.log(position);
+    socket.emit('createLocationMessage',{
+      latitude : position.coords.latitude,
+      longitude : position.coords.longitude
+    })
+  },function () {
+    alert('Unable to fetch location.');
+  })
+});
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,26 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage',() => {
   it('should generate correct message object',() => {
-      var from = 'Piyush'
-      var text = 'This is no war'
+      var from = 'Piyush';
+      var text = 'This is no war';
       var message = generateMessage(from,text);
       expect(message.createdAt).toBeA('number');
       expect(message).toInclude({from,text})
   });
-  
+
+});
+
+describe('generateLocationMessage',() => {
+  it('should generate correct Location object',() => {
+    var from = 'Piyush';
+    var latitude = 1 ;
+    var longitude = 1 ;
+    var url= generateLocationMessage(from,latitude,longitude);
+    expect(url.createdAt).toBeA('number');
+    //expect(url).toInclude({from,url});
+    expect(url.url).toEqual(`https://www.google.com/maps?q=${latitude},${longitude}`);
+  });
 });
